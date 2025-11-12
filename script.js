c6c8244542470328ad58784ac0b5d420354b90d8
lucitey119 <leepriest715@gmail.com>
2025-11-12 15:48:24 +0100

    Add welcome alert and page load logging

---
+++ script.js
@@ -0,0 +1,8 @@
+function sayHello() {
+  alert("Welcome to My Business Website!");
+}
+
+// Optional: log when page loads
+window.onload = function() {
+  console.log("Website loaded successfully!");
+};

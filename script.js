f1386f76d77acb5522c4d486f5310754181d7f30
lucitey119 <leepriest715@gmail.com>
2025-11-13 11:43:10 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function sayHello() {
-  alert("Wecome to My Business Website!");
-}
-
-// Optional: log when page loads
-window.onload = function() {
-  console.log("Website loaded successfully!");
-};
